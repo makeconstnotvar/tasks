9475534d8bf49a4ca66b84c322db97c05a4b68d7
Alexander <akotlyachkov@gmail.com>
2020-04-16 00:34:52 +0300

    task 2 example

---
+++ stage2/example/app/components/User.jsx
@@ -0,0 +1,11 @@
+const User = props => (
+  <div className={`user`}>
+    <div className="foto">
+      <img src={props.user.avatar_url} alt=""/>
+    </div>
+    <div className="info">
+      <div className="name">{props.user.login}</div>
+    </div>
+  </div>
+)
+export {User}
